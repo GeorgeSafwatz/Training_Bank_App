a4859e20f1d9f80829b361ef9c88055d457dee4d
george.safwat <george.safwat@DESKTOP-8RILLND>
2024-08-19 14:37:47 +0300

    Adding new Feature: Transaction

---
+++ Bank App/GlobalUse.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>;
+
+using namespace std;
+struct stClient
+{
+	string accountNumber;
+	string postalCode;
+	string name;
+	string phoneNum;
+	unsigned int accountBalance;
+};
